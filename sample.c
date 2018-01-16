b587e9bfdbea2ab08b20e02daac3c5a56418755b
Takafumi Hirata <takhirata6@gmail.com>
2018-01-16 22:43:08 +0900

    update

--- sample.c
+++ sample.c
@@ -36,6 +36,7 @@ static void save(const char *filename) {
     uint64_t vmsize = 4096;
     uint64_t ncmds = 0;
     /* uint64_t vmsize = 0x0000000000000400; */
+    uint64_t filesize = 1024;
 
     mach_header_t *header = (mach_header_t*)mem + offset;
     header->magic = MH_MAGIC_64;
@@ -51,95 +52,100 @@ static void save(const char *filename) {
     seg1->cmd = LC_SEGMENT_64;
     seg1->cmdsize = sizeof(segment_command_t);
     strcpy(seg1->segname, SEG_PAGEZERO);
-    seg1->vmaddr = 0;
-    seg1->vmsize = 0;//vmsize;
-    seg1->fileoff = 0;
-    seg1->filesize = 0;
-    seg1->maxprot = (vm_prot_t)0x0;
-    seg1->initprot = (vm_prot_t)0x0;
-    seg1->nsects = 0;
-    seg1->flags = 0;
 
     offset += (sizeof(segment_command_t));
     ncmds++;
     printf("2 offset %llu\n", offset);
-    segment_command_t *seg2 = (segment_command_t*)(mem + offset);
-    seg2->cmd = LC_SEGMENT_64;
-    seg2->cmdsize = sizeof(segment_command_t) + sizeof(section_t);
-    strcpy(seg2->segname, SEG_TEXT);
-    seg2->vmaddr = vmaddr;
-    seg2->vmsize = 0x10;//vmsize; // 0x0000000000002000;
-    seg2->fileoff = 0;//start_of_data; // sizeof(mach_header_t) + sizeof(segment_command_t) * 2 + sizeof(entry_point_command_t);
-    /* seg2->filesize = 0x120; */
-    seg2->filesize = 0x10;
-    seg2->maxprot = (vm_prot_t)0x7;
-    seg2->initprot = (vm_prot_t)0x5;
-    seg2->nsects = 1;
-    seg2->flags = 0;
+
+    // {{{ TEXT SEGMENT
+    segment_command_t *text_seg = (segment_command_t*)(mem + offset);
+    text_seg->cmd = LC_SEGMENT_64;
+    text_seg->cmdsize = sizeof(segment_command_t) + sizeof(section_t);
+    strcpy(text_seg->segname, SEG_TEXT);
+    text_seg->vmaddr = vmaddr;
+    text_seg->vmsize = vmsize; // 0x0000000000002000;
+    text_seg->fileoff = 0;//start_of_data; // sizeof(mach_header_t) + sizeof(segment_command_t) * 2 + sizeof(entry_point_command_t);
+    text_seg->filesize = 0; // filesize;//0x10;
+    text_seg->maxprot = VM_PROT_READ | VM_PROT_WRITE | VM_PROT_EXECUTE;
+    text_seg->initprot = VM_PROT_READ | VM_PROT_EXECUTE;
+    text_seg->nsects = 1;
+    text_seg->flags = 0;
+    /// TEXT SEGMENT }}}
+
 
     offset += (sizeof(segment_command_t));
     printf("3 offset %llu\n", offset);
 
+    /// {{{ TEXT SECTION
     section_t *text_section = (section_t*)(mem + offset);
-    strcpy(text_section->sectname, "__text");
-    strcpy(text_section->segname, "__TEXT");
-    printf("start_of_data %llx\n", PLACEHOLDER);
-    text_section->addr = vmaddr + PLACEHOLDER;
-    text_section->size = 0x10;
-    text_section->offset = PLACEHOLDER;
+    strcpy(text_section->sectname, SECT_TEXT);
+    strcpy(text_section->segname, SEG_TEXT);
     text_section->align = 4;
     text_section->nreloc = 0;
     text_section->flags = S_ATTR_PURE_INSTRUCTIONS |  S_ATTR_SOME_INSTRUCTIONS;
-
-    printf("vmaddr + vmsize = %llx\n", seg2->vmaddr + seg2->vmsize);
-    printf("addr + size = %llx\n", text_section->addr + text_section->size);
+    // TEXT SECTION }}}
 
     offset += (sizeof(section_t));
     ncmds++;
     printf("4 offset %llu\n", offset);
+
+    /// {{{ ENTRY POINT
     entry_point_command_t *entry = (entry_point_command_t*)(mem + offset);
     entry->cmd = LC_MAIN;
     entry->cmdsize = 24;
     entry->entryoff = 0;
     entry->stacksize = 0;
+    //  ENTRY POINT}}}
 
     offset += sizeof(entry_point_command_t);
     ncmds++;
+
+    /// {{{ DYLINKER COMMAND
     dylinker_command_t *dylinker = (dylinker_command_t*)(mem + offset);
-    dylinker->cmd = LC_LOAD_DYLINKER + 20;
-    dylinker->cmdsize = sizeof(dylinker_command_t);
+    dylinker->cmd = LC_LOAD_DYLINKER;
+    dylinker->cmdsize = sizeof(dylinker_command_t) + 20 + 8;
     union lc_str name = { 0x0c };
     dylinker->name = name;
     offset += sizeof(dylinker_command_t);
-    strncpy((char*)(mem+offset), "/usr/lib/dyld", 20);
+    strncpy((char*)(mem+offset), "/usr/lib/dyld", 20 + 8);
+    // DYLINKER COMMAND }}}
 
+    offset += 20 + 8;
 
+    // fill header
     header->ncmds = ncmds;
     header->sizeofcmds = offset - sizeof(mach_header_t);
 
-    offset += 20;
+    // fill textsegment
+    printf("offset %llud\n", offset);
+    text_seg->filesize = offset;
+
+    // fill text_section
+    text_section->addr = vmaddr + offset;
+    text_section->size = 0x10;
+    text_section->offset = offset;
+
+    // fill entry
+    entry->entryoff = offset;
+
+    uint64_t vm_end = text_seg->vmaddr + text_seg->vmsize;
+    uint64_t section_end = text_section->addr + text_section->size;
+    printf("vmaddr + vmsize = %llx\n", text_seg->vmaddr + text_seg->vmsize);
+    printf("addr + size = %llx\n", text_section->addr + text_section->size);
+
+    if (section_end > vm_end) {
+        printf("failed\n");
+        return;
+    }
+
+
     printf("5 offset %llu\n", offset);
-    mem[offset++] = 0x00;
-    mem[offset++] = 0x55;
-    mem[offset++] = 0x48;
-    mem[offset++] = 0x89;
-    mem[offset++] = 0xE5;
-    mem[offset++] = 0x31;
-    mem[offset++] = 0xC0;
-    mem[offset++] = 0xC7;
-    mem[offset++] = 0x45;
-    mem[offset++] = 0xFC;
-    mem[offset++] = 0x00;
-    mem[offset++] = 0x00;
-    mem[offset++] = 0x00;
-    mem[offset++] = 0x00;
-    mem[offset++] = 0x5D;
-    mem[offset++] = 0xC3;
+    uint8_t prog[16] = {0x55,0x48,0x89,0xE5,0x31,0xC0,0xC7,0x45,0xFC,0x00,0x00,0x00,0x00,0x5D,0xC3, 0x00};
+    memcpy(mem+offset, prog, 16);
 
     FILE *fp = fopen(filename, "wb");
     /* fwrite(mem, 1, 0x200, fp); */
-    printf("start_of_data %llu\n", PLACEHOLDER);
-    /* fwrite(mem, 1, start_of_data + 8, fp); */
+    printf("start_of_data %llu\n", offset);
     fwrite(mem, 1, 4096, fp);
     fclose(fp);
     chmod(filename, 0777);
